a7f80e94fc78055beaee7171dada75db7dfa6a39
Gedeon Eyasu <gedyeyasu@gmail.com>
2022-01-30 16:09:38 +0300

    finished styling ui

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -9,17 +9,42 @@ interface Props {
 }
 const TodoList: React.FC<Props> = ({ todos, setTodos }) => {
   return (
-    <div className="todos">
-      {todos.map((todo) => (
-        <SingleTodo
-          todo={todo}
-          key={todo.id}
-          todos={todos}
-          setTodos={setTodos}
-        />
-        // <li>{todo.todo}</li>
-      ))}
+    <div className="container">
+      <div className="todos">
+        <span className="todosHeading">Active Tasks</span>
+        {todos.map((todo) => (
+          <SingleTodo
+            todo={todo}
+            key={todo.id}
+            todos={todos}
+            setTodos={setTodos}
+          />
+        ))}
+      </div>
+      <div className="todos done">
+        <span className="todosHeading">Completed Tasks</span>
+        {todos.map((todo) => (
+          <SingleTodo
+            todo={todo}
+            key={todo.id}
+            todos={todos}
+            setTodos={setTodos}
+          />
+          // <li>{todo.todo}</li>
+        ))}
+      </div>
     </div>
+    // <div className="todos">
+    //   {todos.map((todo) => (
+    //     <SingleTodo
+    //       todo={todo}
+    //       key={todo.id}
+    //       todos={todos}
+    //       setTodos={setTodos}
+    //     />
+    //     // <li>{todo.todo}</li>
+    //   ))}
+    // </div>
   );
 };
 
